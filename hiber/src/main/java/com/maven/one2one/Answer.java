be18718fae89d80725eed85b8cfd6406ec35be0c
Priye Ranjan <anupam.ranjan4u@gmail.com>
2021-05-19 20:07:48 +0530

    One to One Relation

---
+++ hiber/src/main/java/com/maven/one2one/Answer.java
@@ -0,0 +1,44 @@
+package com.maven.one2one;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int answer_id;
+	private String answer;
+	
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Answer(int answer_id, String answer) {
+		super();
+		this.answer_id = answer_id;
+		this.answer = answer;
+	}
+	public int getAnswer_id() {
+		return answer_id;
+	}
+	public void setAnswer_id(int answer_id) {
+		this.answer_id = answer_id;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	@Override
+	public String toString() {
+		return "Answer [answer_id=" + answer_id + ", answer=" + answer + "]";
+	}
+	
+	
+	
+}
---
+++ hiber/src/main/java/com/maven/one2one/MapDemo1.java
@@ -0,0 +1,46 @@
+package com.maven.one2one;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class MapDemo1 {
+	public static void main(String[] args) {
+		SessionFactory factory=new Configuration().configure().buildSessionFactory();
+		
+		Question q1=new Question();
+		//q1.setQuestion_id(121);
+		q1.setQuestion("How many continents in world");
+		
+		Answer ans1=new Answer();
+		//ans1.setAnswer_id(201);
+		ans1.setAnswer("7");
+		
+		q1.setAnswer(ans1);
+		
+		Question q2=new Question();
+	//	q2.setQuestion_id(122);
+		q2.setQuestion("Which is the largest country?");
+		
+		Answer ans2=new Answer();
+		//ans2.setAnswer_id(202);
+		ans2.setAnswer("Russia");
+		
+		q2.setAnswer(ans2);
+		
+		Session session=factory.openSession();
+        Transaction tx=session.beginTransaction();
+        session.save(q1);
+        session.save(q2);
+        
+        session.save(ans1);
+        session.save(ans2);
+        
+        tx.commit();
+        session.close();
+		
+		
+		
+	}
+}
